258ecfa736ee1fa841ed28458fc3a00873d8591d
Mick R <mickriego214@gmail.com>
2021-06-30 11:08:19 +0400

    addsitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,13 +12,6 @@ module.exports = {
       },
     },
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
