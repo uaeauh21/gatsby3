28f03e1df61411bba6143aae412b3c4747209430
Mick R <mickriego214@gmail.com>
2021-07-01 12:18:25 +0400

    Finished Gatsby Tutorial Part 2

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,10 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-gatsby-cloud/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-image/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[],"trackingId":"277549400","head":false,"anonymize":false,"respectDNT":false,"exclude":[],"pageTransitionDelay":0,"enableWebVitalsTracking":false},
+    }]
---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\Michael\\gatsby\\gatsby3\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\Michael\\gatsby\\gatsby3\\src\\pages\\404.js")),
+  "component---src-pages-about-js": preferDefault(require("C:\\Users\\Michael\\gatsby\\gatsby3\\src\\pages\\about.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\Michael\\gatsby\\gatsby3\\src\\pages\\index.js"))
+}
+
